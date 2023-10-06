5f77a94060b7d886bc56d833df1d500ec8ff0a8b
Dhruvil Dobariya <127285714+Dhruvil-Dobariya-30@users.noreply.github.com>
2023-10-06 17:53:56 +0530

    Add files via upload

--- DE NEW/Navbar-Stu/index.js
+++ DE NEW/Navbar-Stu/index.js
@@ -1,3 +1,19 @@
+// const hamburger = document.querySelector(".hamburger");
+// const ul = document.querySelector("ul");
+
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle('active')
+//   ul.classList.toggle('active')
+//   ul.classList.toggle("ham_active");
+
+//   let ham_child = hamburger.firstElementChild;
+//   if (ul.classList.contains("ham_active")) {
+//     ham_child.classList.replace("fa-bars", "fa-xmark");
+//   } else {
+//     ham_child.classList.replace("fa-xmark", "fa-bars");
+//   }
+// });
+
 const hamburger = document.querySelector(".hamburger");
 const ul = document.querySelector("ul");
 const ham_active = document.querySelector(".ham_active");
--- DE NEW/form/script.js
+++ DE NEW/form/script.js
@@ -5,11 +5,16 @@ const signinBtn = document.getElementById("signinBtn");
 const signupBtn = document.getElementById("signupBtn");
 const signup = document.getElementsByClassName("signup");
 
+// nameField.addEventListener("input", function () {
+//   this.style.color = "#000";
+// });
+
 signupBtn.onclick = function () {
   title.innerHTML = "Sign Up";
   p.innerHTML = "Have Account? <a href='./index.html' >Login</a>";
-    nameField.style.display = "block";
+  nameField.style.display = "block";
 };
+
 signinBtn.onclick = function () {
   title.innerHTML = "Login";
   nameField.style.display = "none";
